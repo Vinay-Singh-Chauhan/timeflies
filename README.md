4a76a8f0d7a4fff42bde93f70ced1eb71545cecf
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2025-01-13 18:22:35 +0530

    setup electron forge configs for build

---
+++ README.md
@@ -0,0 +1,15 @@
+# TimeFlies
+
+## Introduction
+This is a calendar type widget for your desktop which shows you current local date and time along with percentage of the year that has been passed.
+
+Bug reports, suggestions and feature enhancements are welcome!
+## Notice regarding Intellectual Property
+Ownership of anything and everything used in this project lies with their respective owners. Following are the attributions that I can recall for now (If I miss any please raise an issue or a PR).
+
+1. Electron: used to package and create this application
+2. favicon.io: took a favicon for the application from here. Details: (./assets/about)
+3. Electron Publisher: used for publishing this application
+
+## Screenshots:
+
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { app, BrowserWindow } = require('electron/main')
-
+if (require('electron-squirrel-startup')) app.quit();
 const createWindow = () => {
   const win = new BrowserWindow({
+    // frame:false,
+    autoHideMenuBar:true,
+    minimizable:false,
+    titleBarStyle: 'customButtonsOnHover',
+    titleBarOverlay:true,
+    useContentSize:true,
+    opacity:1,
+    closable:false,
+    resizable:false,
+    maximizable:false,
+    // x:0,
+    // y:0,
+    
+    skipTaskbar:true,
+    icon:"./assets/favicon.ico",
     width: 400,
     height: 200
   })
@@ -10,13 +25,23 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-  createWindow()
+   createWindow()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
+
   })
+  app.on('show', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+     createWindow()
+    }
+    
+
+    
+  })
+  
 })
 
 app.on('window-all-closed', () => {
